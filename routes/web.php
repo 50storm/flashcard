00c046efb296dd49d9cddd281e745a17907e5b09
Hiroshi Igarashi <hirotech.net@gmail.com>
2024-09-30 19:54:36 +0900

    パスワードリセットのPOSTルートをオーバーライドしてカスタマイズ可能にする

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// パスワードリセットのPOSTルート
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 Auth::routes();
 
