48c42ad3c0dce38d4a19fdf915d5971154e3cf5b
Hiroshi Igarashi <hirotech.net@gmail.com>
2024-10-12 15:11:30 +0900

    Bootstrap 5 JSを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>フラッシュカード</title>
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Section for Page-specific Styles -->
     @yield('styles')
 </head>
