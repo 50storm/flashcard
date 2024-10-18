48e70920c68d040b1fc9fa9e52ed007da280c461
Hiroshi Igarashi <hirotech.net@gmail.com>
2024-10-18 09:50:50 +0900

    Languageモデルを編集

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Language extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['language', 'language_code'];
+
+    /**
+     * Get the contents for the language.
+     * 言語に関連する複数のコンテンツを取得するリレーション
+     */
+    public function contents()
+    {
+        return $this->hasMany(Content::class, 'language_id', 'id');
+    }
 }
