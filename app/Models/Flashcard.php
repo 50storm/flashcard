ae5ba6cd3d874ea8014cdae4aabd053e4bc46757
Hiroshi Igarashi <hirotech.net@gmail.com>
2024-10-20 17:58:22 +0900

    Flashcardモデル修正

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -9,5 +9,22 @@ class Flashcard extends Model
 {
     use HasFactory;
 
+    // 一括割り当て可能な属性
     protected $fillable = ['english', 'japanese'];
+
+    /**
+     * フラッシュカードに関連するコンテンツを取得
+     */
+    public function contents()
+    {
+        return $this->hasMany(FlashcardContent::class, 'flashcard_id');
+    }
+
+    /**
+     * フラッシュカードに関連するタグを取得（多対多リレーション）
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'flashcard_tag', 'flashcard_id', 'tag_id');
+    }
 }
